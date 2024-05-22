5ae59481998ee3a510f9d6b475e0fd9dad540208
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-22 20:32:42 -0300

    Questao 01

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,19 @@
 const prompt = require('prompt-sync')();
+
+function calcularPerdaVida(cigarrosPorDia, anosFumando){
+    let minutosCigarros = 10
+    let minutosPorDia = 24 * 60
+
+    let total = cigarrosPorDia * minutosCigarros * 365 * anosFumando
+
+    let diasPerdidos = total / minutosPorDia
+
+    return diasPerdidos
+}
+
+let cigarrosPorDia = parseInt(prompt("Quantos cigarros por dia?"))
+let anosFumando = parseInt(prompt("Quantos anos fumando?"))
+
+let diasPerdidos = calcularPerdaVida(cigarrosPorDia, anosFumando)
+
+console.log("Você perderá " + diasPerdidos.toFixed(2) + " dias de vida")
