fad60d01c9aefde405d03da0193fa5e52a812e3c
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-22 20:46:22 -0300

    questao 02

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,17 @@
 const prompt = require('prompt-sync')();
+
+function calcularMulta(velocidade){
+    let limite = 80
+    let multa = (velocidade - limite) * 5
+
+    return multa
+}
+
+let velocidade = parseInt(prompt("Qual a velocidade do carro?"))
+
+if(velocidade > 80){
+    let multa = calcularMulta(velocidade)
+    console.log("Você foi multado! O valor da Multa é R$" + multa)
+}else{
+    console.log("Você não foi multado!")
+}
