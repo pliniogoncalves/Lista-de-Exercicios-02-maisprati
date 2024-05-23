6ad32b0d60f16ede9d867a77f47e3a6478e0f5fd
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-22 21:28:19 -0300

    Questao 04

--- 04/index.js
+++ 04/index.js
@@ -1,1 +1,15 @@
 const prompt = require('prompt-sync')();
+
+function podeFormarTriangulo(a, b, c) {
+    return a < b + c && b < a + c && c < a + b;
+}
+
+let segmento1 = parseInt(prompt("Digite o tamanho do primeiro segmento:"))
+let segmento2 = parseInt(prompt("Digite o tamanho do segundo segmento:"))
+let segmento3 = parseInt(prompt("Digite o tamanho do terceiro segmento:"))
+
+if (podeFormarTriangulo(segmento1, segmento2, segmento3)) {
+    console.log('Os segmentos podem formar um triângulo.');
+  } else {
+    console.log('Os segmentos não podem formar um triângulo.');
+}
