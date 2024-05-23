92604851274c5deafb75bfbb21e2d0f8006b295f
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-22 21:09:42 -0300

    Questao 02 e 03

--- 02/index.js
+++ 02/index.js
@@ -4,14 +4,12 @@ function calcularMulta(velocidade){
     let limite = 80
     let multa = (velocidade - limite) * 5
 
-    return multa
+    if(velocidade > 80){
+        return console.log("Você foi multado! O valor da Multa é R$" + multa)
+    }
+
+    return console.log("Você não foi multado!")
 }
 
 let velocidade = parseInt(prompt("Qual a velocidade do carro?"))
-
-if(velocidade > 80){
-    let multa = calcularMulta(velocidade)
-    console.log("Você foi multado! O valor da Multa é R$" + multa)
-}else{
-    console.log("Você não foi multado!")
-}
+calcularMulta(velocidade)
--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,16 @@
 const prompt = require('prompt-sync')();
+
+function calculoPassagem(distancia){
+    let preco = 0
+
+    if(distancia<=200){
+        preco = distancia * 0.5
+        return console.log("O preço da passagem é R$"+ preco)
+    }
+
+    preco = distancia * 0.45
+    return console.log("O preço da passagem é R$"+ preco)
+}
+
+let distancia = parseFloat(prompt("Qual a distancia da viagem?"))
+calculoPassagem(distancia)
